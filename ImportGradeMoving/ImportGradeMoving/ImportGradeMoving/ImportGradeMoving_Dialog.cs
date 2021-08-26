134c1f1fc4e6ee0be069b9c2e790e1b18d165032
Nuttapont2538 <nuttapont.t@kkumail.com>
2021-08-26 14:23:29 +0700

    commit

--- ImportGradeMoving/ImportGradeMoving/ImportGradeMoving/ImportGradeMoving_Dialog.cs
+++ ImportGradeMoving/ImportGradeMoving/ImportGradeMoving/ImportGradeMoving_Dialog.cs
@@ -59,7 +59,7 @@ namespace ImportGradeMoving
                     rtbLogDataImportPreparing.Clear();
                     rtbLogDataImportPreparing.AppendText("ไม่สามารถเปลี่ยนเกรด ที่เป็นเกรดเดิมได้");
                 }
-                //
+                //testttttttt
 
             }
             catch (Exception ex)
